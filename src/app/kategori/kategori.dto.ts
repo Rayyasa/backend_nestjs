48e0f0b03287cc849ba91528e70f75d34b930471
Rayyasa <rayya.a192018@gmail.com>
2024-01-10 21:35:20 +0700

    bab kategori kelar

--- src/app/kategori/kategori.dto.ts
+++ src/app/kategori/kategori.dto.ts
@@ -1,18 +1,32 @@
-import { OmitType } from '@nestjs/mapped-types';
-import { IsInt, IsOptional, IsString } from 'class-validator';
-import { PageRequestDto } from 'src/utils/dto/page.dto';
+import { OmitType } from "@nestjs/mapped-types";
+import { IsInt,IsObject,IsOptional,IsString } from "class-validator";
+import { PageRequestDto } from "src/utils/dto/page.dto";
 
 export class KategoriDto {
   @IsInt()
   id?: number;
 
   @IsString()
-  nama_kategori: string;
-}
+  nama_kategori:string;
 
-export class CreateKategoriDto extends OmitType(KategoriDto, ['id']) {}
-export class findAllKategori extends PageRequestDto {
+  @IsObject()
+  @IsOptional()
+  created_by: {id:number}
+
+  @IsObject()
+  @IsOptional()
+  updated_by: {id:number}
+}
+export class CreateKategoriDto extends OmitType(KategoriDto,['id']) {}
+export class UpdateKategoriDto extends OmitType(KategoriDto, ['id']) {} 
+export class FindAllKategori extends PageRequestDto {
   @IsString()
   @IsOptional()
   nama_kategori?: string;
+}
+export class CreateKategoriArrayDto {
+  @IsArray()
+  @ValidateNested({each: true})
+  @Type(() => CreateKategoriDto)
+  data: CreateKategoriDto[];
 }
--- src/app/kategori/kategori.service.ts
+++ src/app/kategori/kategori.service.ts
@@ -1,10 +1,17 @@
-import { HttpException, HttpStatus, Inject, Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Inject, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import BaseResponse from 'src/utils/response/base.response';
+<<<<<<< HEAD
 import { Kategori } from './kategori.entity';
 import { CreateKategoriDto, findAllKategori } from './kategori.dto';
 import { ResponsePagination, ResponseSuccess } from 'src/interface';
 import { Like, Repository } from 'typeorm';
+=======
+import { kategori } from './kategori.entity';
+import { CreateKategoriArrayDto, CreateKategoriDto, FindAllKategori, UpdateKategoriDto } from './kategori.dto';
+import { ResponsePagination, ResponseSucces } from 'src/interface/response';
+import { Like, Repository } from 'typeorm'
+>>>>>>> 76fee98 (bab kategori kelar)
 import { REQUEST } from '@nestjs/core';
 
 @Injectable()
@@ -19,6 +26,7 @@ export class KategoriService extends BaseResponse {
 
   async create(payload: CreateKategoriDto): Promise<ResponseSuccess> {
     try {
+<<<<<<< HEAD
       await this.kategoriRepository.save({
         ...payload,
         created_by: {
@@ -27,12 +35,62 @@ export class KategoriService extends BaseResponse {
       });
 
       return this._Success('OK', this.req.user.user_id);
+=======
+      await this.kategoriRepository.save(payload);
+      return this._success('Oke', payload.created_by.id);
+>>>>>>> 76fee98 (bab kategori kelar)
     } catch {
       throw new HttpException('Ada Kesalahan', HttpStatus.UNPROCESSABLE_ENTITY);
     }
   }
 
+<<<<<<< HEAD
   async getAllCategory(query: findAllKategori): Promise<ResponsePagination> {
+=======
+  async updateKategori(payload: UpdateKategoriDto, id: number): Promise<ResponseSucces> {
+    const checkKategori = await this.kategoriRepository.findOne({ where: { id: id } });
+
+    if (!checkKategori) {
+      throw new HttpException('Kategori tidak ditemukan', HttpStatus.NOT_FOUND)
+    }
+    const updateKategori = await this.kategoriRepository.save({
+      ...payload, id: id
+    });
+    return this._success('Berhasil mengUpdate', updateKategori)
+  }
+
+  async getDetail(id: number): Promise<ResponseSucces> {
+    const detailKategori = await this.kategoriRepository.findOne({
+      where: {
+        id,
+      }, 
+      relations: ['created_by', 'updated_by'],
+      select: {
+        id: true,
+        nama_kategori: true,
+        created_by: {
+          id: true, // pilih field  yang akan ditampilkan dari tabel user
+          nama: true,
+        },
+        updated_by: {
+          id: true, // pilih field yang akan ditampilkan dari tabel user
+          nama: true,
+        },
+      },
+    });
+
+    if (detailKategori === null) {
+      throw new NotFoundException(`Kategori dengan id ${id} tidak ditemukan`);
+    } return {
+      status: 'Success',
+      message: 'Detail kategori ditemukan',
+      data: detailKategori,
+    }
+  }
+
+
+  async getAllCategory(query: FindAllKategori): Promise<ResponsePagination> {
+>>>>>>> 76fee98 (bab kategori kelar)
     const { page, pageSize, limit, nama_kategori } = query;
 
     const filterQuery: { [key: string]: any } = {}
@@ -63,4 +121,48 @@ export class KategoriService extends BaseResponse {
 
     return this._Pagination('OK', result, total, page, pageSize);
   }
+<<<<<<< HEAD
 }
+=======
+
+
+  async deleteKategori(id: number): Promise<ResponseSucces> {
+    const check = await this.kategoriRepository.findOne({
+      where: {
+        id,
+      },
+    });
+
+    if (!check)
+      throw new NotFoundException(`Kategori dengan id ${id} tidak ditemukan`);
+    await this.kategoriRepository.delete(id);
+    return {
+      status: `Success `,
+      message: 'Berhasil menghapus Kategori',
+    };
+  }
+  async bulkCreate(payload: CreateKategoriArrayDto): Promise<ResponseSucces> {
+    try {
+      let berhasil = 0;
+      let gagal = 0;
+      await Promise.all(
+        payload.data.map(async (data) => {
+          try {
+            await this.kategoriRepository.save(data);
+            berhasil += 1;
+          } catch {
+            gagal += 1;
+          }
+        }),
+      )
+      return {
+        status: 'Ok',
+        message: `Berhasil menambahkan kategori sebanyak ${berhasil} dan gagal ${gagal}`
+      }
+    } catch {
+      throw new HttpException('Ada kesalahan', HttpStatus.BAD_REQUEST)
+    }
+  }
+
+}
+>>>>>>> 76fee98 (bab kategori kelar)
